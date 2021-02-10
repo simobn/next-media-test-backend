6967abf8c994e7c105c16ae7d95549a5c624b232
mohammedBenouijem <mbenouijem@gmail.com>
2021-02-10 15:23:36 +0100

    adding guarded and relationships to models

--- app/Category.php
+++ app/Category.php
@@ -6,5 +6,28 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    protected $guarded = [];
+
+    /**
+     * Get the products for the blog category.
+     */
+    public function products()
+    {
+        return $this->hasMany(Product::class);
+    }
+
+
+    /**
+     * Get the category childrens.
+     */
+    public function children(){
+        return $this->hasMany( 'App\Category', 'parent_id', 'id' );
+    }
+
+    /**
+     * Get the ccategory parent.
+     */
+    public function parent(){
+        return $this->hasOne( 'App\Category', 'id', 'parent_id' );
+    }
 }
--- app/Product.php
+++ app/Product.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    protected $guarded = [];
+
+    /**
+     * Get the category that owns the product.
+     */
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
